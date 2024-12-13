4ff79a1ceccba59099a202d63779eaa7b2311ddb
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-12-13 20:11:56 +0000

    feat(misc): config dec;
    - Use config declaration type.

--- ckanext/xloader/jobs.py
+++ ckanext/xloader/jobs.py
@@ -337,7 +337,7 @@ def _download_resource_data(resource, data, logger):
     scheme = url_parts.scheme
 
     # (canada fork only): uploaded and white listed only
-    allowed_domains = config.get('ckanext.canada.datastore_source_domain_white_list', '').split()
+    allowed_domains = config.get('ckanext.canada.datastore_source_domain_allow_list', [])
     if (resource.get('url_type') != 'upload' and url_parts.netloc not in allowed_domains):
         raise JobError('Only uploaded resources and white listed sources can be uploaded to the DataStore.')
 
